0c493d5ef9934d8b6a53da1a561f72f1ce638d3a
Marco A. Rojas <contact@marcorojas.me>
2018-04-14 10:36:32 -0500

    Import trained model as Js

--- parser.js
+++ parser.js
@@ -1,96 +1,93 @@
-var  parser = {};
+let Parser = {}
+const keyWords = ['Robar', 'Robo', 'Seguridad', 'Inseguridad', 'Muerte', 'Accidente', 'Asalto', 'Delincuencia', 'Crimen', 'Automovilistico', 'Automovil', 'Coche', 'Auto', 'Aparatoso', 'Vehículo', 'camioneta', 'Conductor', 'Avenida', 'Tránsito', 'Tráfico', 'Murieron', 'Murió', 'Chofer', 'Calle', 'Heridos', 'Heridas', 'Choque', 'Velocidad', 'Enfermedad', 'Tratamiento', 'Riesgo', 'Delictivos', 'Horrible', 'Depresión', 'Impacto', 'Sufrir', 'muerto', 'muertos', 'delincuentes', 'Ruta', 'SegurosBancomer', 'GNPSeguros', 'Qualitas_MX', 'atropellar', 'atropellan', 'aseguradora', 'víal', 'vial', 'carretera', 'carril', 'av', 'col'];
+const NoKeyWords = ['Meade', 'política', 'chichis', 'candidato', 'elecciones', 'margarita', 'trump', 'ecuador', 'bogota', 'colombia', 'pizza', 'VivirEsIncrible', 'cube', 'tropa', 'victoria', 'anaya', 'RicardoAnayaC', 'JoseAMeadek', 'cubanos', 'Perú']
 
-var key_words = [ 'Robar','Robo','Seguridad','Inseguridad','Muerte','Accidente','Asalto','Delincuencia','Crimen','Automovilistico','Automovil','Coche','Auto','Aparatoso','Vehículo','camioneta','Conductor','Avenida','Tránsito','Tráfico','Murieron','Murió','Chofer','Calle','Heridos','Heridas','Choque','Velocidad','Enfermedad','Tratamiento','Riesgo','Delictivos','Horrible','Depresión','Impacto','Sufrir','muerto','muertos','delincuentes','Ruta','SegurosBancomer','GNPSeguros','Qualitas_MX','atropellar','atropellan','aseguradora','víal','vial','carretera','carril','av','col'];
-var no_key_words = ['Meade', 'política','chichis', 'candidato', 'elecciones', 'margarita','trump', 'ecuador','bogota','colombia','pizza','VivirEsIncrible','cube','tropa','victoria','anaya','RicardoAnayaC','JoseAMeadek','cubanos','Perú'];
+Parser.formatTrainingSet = function(trainingSet) {
 
+  var formattedSet = [];
+  for (var i = 0; i < trainingSet.length; i++) {
+    trainingSet[i].tweet = trainingSet[i].tweet.replace(/, /g ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace('.' ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace('/' ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace(':' ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace('!' ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace('?' ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace('¿' ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace('¡' ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace(/@/g ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.replace(/#/g ,  '');
+    trainingSet[i].tweet = trainingSet[i].tweet.toLowerCase();
 
-parser.formatTrainingSet = function( trainingSet ){
+    var splited = trainingSet[i].tweet.split(" ");
+    trainingSet[i].tweet = splited
 
-	var formattedSet = [];
-	for (var i = 0; i < trainingSet.length; i++) {
+    var input = {};
+    for (var j = 0; j < trainingSet[i].tweet.length; j++) {
+      for (var k = 0; k < keyWords.length; k++) {
+        //console.log( keyWords[k].toLowerCase() + ' ' + trainingSet[i].tweet[j] );
+        if(keyWords[k].toLowerCase().toString() === trainingSet[i].tweet[j].toString() ){
+          input[trainingSet[i].tweet[j]] = 1;
+          break;
+        }
+        else{
+          input[trainingSet[i].tweet[j]] = 0.5;
+        }
 
-		trainingSet[i].tweet = trainingSet[i].tweet.replace(/,/g , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace('.' , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace('/' , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace(':' , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace('!' , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace('?' , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace('¿' , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace('¡' , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace(/@/g , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.replace(/#/g , '');
-		trainingSet[i].tweet = trainingSet[i].tweet.toLowerCase();
+      }
+      for (var l = 0; l < NoKeyWords.length; l++) {
+        //console.log( keyWords[k].toLowerCase() + ' ' + trainingSet[i].tweet[j] );
+        if(NoKeyWords[l].toLowerCase().toString() === trainingSet[i].tweet[j].toString() ){
+          input[trainingSet[i].tweet[j]] = 0;
+          break;
+        }
+      }
+    }
 
-		var splited = trainingSet[i].tweet.split(" ");
-		trainingSet[i].tweet = splited
+    formattedSet.push({input: input, output: trainingSet[i].predict });
 
-		var input = {};
-		for (var j = 0; j < trainingSet[i].tweet.length; j++) {
-			for (var k = 0; k < key_words.length; k++) {
-				//console.log( key_words[k].toLowerCase() + ' ' + trainingSet[i].tweet[j] );
-				if(key_words[k].toLowerCase().toString() == trainingSet[i].tweet[j].toString() ){
-					input[trainingSet[i].tweet[j]] = 1;
-					break;
-				}
-				else{
-					input[trainingSet[i].tweet[j]] = 0.5;
-				}
-				
-			}
-			for (var k = 0; k < no_key_words.length; k++) {
-				//console.log( key_words[k].toLowerCase() + ' ' + trainingSet[i].tweet[j] );
-				if(no_key_words[k].toLowerCase().toString() == trainingSet[i].tweet[j].toString() ){
-					input[trainingSet[i].tweet[j]] = 0;
-					break;
-				}
-			}
-		}
+  }
 
-		formattedSet.push({input: input, output: trainingSet[i].predict });
-
-	}
-
-	return formattedSet;
+  return formattedSet;
 };
 
 
-parser.formatTestSet = function (testSet){
+Parser.formatTestSet = function (testSet){
 
 
-	testSet.tweet = testSet.tweet.replace(/,/g , '');
-	testSet.tweet = testSet.tweet.replace(/@/g , '');
-	testSet.tweet = testSet.tweet.replace(/#/g , '');
-	testSet.tweet = testSet.tweet.replace('_' , ' ');
-	testSet.tweet = testSet.tweet.replace('_' , ' ');
-	testSet.tweet = testSet.tweet.toLowerCase();
+  testSet.tweet = testSet.tweet.replace(/,/g , '');
+  testSet.tweet = testSet.tweet.replace(/@/g , '');
+  testSet.tweet = testSet.tweet.replace(/#/g , '');
+  testSet.tweet = testSet.tweet.replace('_' , ' ');
+  testSet.tweet = testSet.tweet.replace('_' , ' ');
+  testSet.tweet = testSet.tweet.toLowerCase();
 
-	var splited = testSet.tweet.split(" ");
-	testSet.tweet = splited
+  var splited = testSet.tweet.split(" ");
+  testSet.tweet = splited
 
-	var input = {};
-	for (var j = 0; j < testSet.tweet.length; j++) {
-		for (var k = 0; k < key_words.length; k++) {
-			//console.log( key_words[k].toLowerCase() + ' ' + testSet.tweet[j] );
-			if(key_words[k].toLowerCase().toString() == testSet.tweet[j].toString() ){
-				input[testSet.tweet[j]] = 1;
-				break;
-			}
-			else{
-				input[testSet.tweet[j]] = 0.5;
-			}
-			
-		}
-		for (var k = 0; k < no_key_words.length; k++) {
-			//console.log( key_words[k].toLowerCase() + ' ' + testSet.tweet[j] );
-			if(no_key_words[k].toLowerCase().toString() == testSet.tweet[j].toString() ){
-				input[testSet.tweet[j]] = 0;
-				break;
-			}
-		}
-	}
+  var input = {};
+  for (var j = 0; j < testSet.tweet.length; j++) {
+    for (var k = 0; k < keyWords.length; k++) {
+      //console.log( keyWords[k].toLowerCase() + ' ' + testSet.tweet[j] );
+      if(keyWords[k].toLowerCase().toString() === testSet.tweet[j].toString() ){
+        input[testSet.tweet[j]] = 1;
+        break;
+      }
+      else{
+        input[testSet.tweet[j]] = 0.5;
+      }
 
-	return input;
+    }
+    for (var m = 0; m < NoKeyWords.length; m++) {
+      //console.log( keyWords[k].toLowerCase() + ' ' + testSet.tweet[j] );
+      if(NoKeyWords[m].toLowerCase().toString() === testSet.tweet[j].toString() ){
+        input[testSet.tweet[j]] = 0;
+        break;
+      }
+    }
+  }
+
+  return input;
 
 };
 
-module.exports = parser;
+module.exports = Parser;
